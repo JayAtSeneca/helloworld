5a0011606a3249d08eea35450872c377c8ac0049
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-05-19 15:28:27 -0400

    updates server.js file

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Jay Pravinkumar Chaudhari Student ID: 147268205 Date: 2022-05-19
+*
+*  Heroku Web App URL: https://shrouded-temple-48685.herokuapp.com/
+*
+*  GitHub Repository URL: https://github.com/JayAtSeneca/helloworld
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
